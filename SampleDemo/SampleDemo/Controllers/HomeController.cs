e6945eefc66e355bbe4c78693e54939c7a39de31
asif-m22 <39861034+asif-m22@users.noreply.github.com>
2018-06-18 01:46:02 +0530

    Update HomeController.cs

--- SampleDemo/SampleDemo/Controllers/HomeController.cs
+++ SampleDemo/SampleDemo/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-/* code changes from GIT Hub */
+/* code changes */
 namespace SampleDemo.Controllers
 {
     public class HomeController : Controller
